e9cd2323b6aa72fbe27032f4e488556c32bd9e99
langat justus <justuslangat78@gmail.com>
2020-11-03 10:52:02 +0300

    started the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    PasswordResetComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
