bcb2fdeb697a0073b46d875326007657f96a90c2
langat justus <justuslangat78@gmail.com>
2020-11-03 20:25:56 +0300

    signup functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
-import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     SignupComponent,
+    LoginComponent,
+    LogoutComponent,
     PasswordResetComponent,
-    LogoutComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
